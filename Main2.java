75bcbf1321558325794ae7d7b844111660a89a0f
scream4ik89 <scream4ik89@yandex.ru>
2017-11-12 23:34:42 +0300

    2

--- Main2.java
+++ Main2.java
@@ -1,6 +1,9 @@
 import sun.text.normalizer.Replaceable;
 
 public class Main2 {
+    public Main2() {
+    }
+
     public static void main(String[] arg){
         String text = ("Первое занятие в парке высоких технологий!"); // 1. задание
         int size = text.length();
@@ -8,8 +11,8 @@ public class Main2 {
         System.out.println("1. задание:  " + text2);
         String text3 = text.substring(21, 42);
         System.out.println("             " + text3);
-        int x = 1027; // 2. задание
-        if (x % 10 == 7){
+        int f = 1027; // 2. задание
+        if (f % 10 == 7){
                 System.out.println("2. задание:  " + "есть цифра 7");
         }
         else {
@@ -30,7 +33,9 @@ public class Main2 {
         System.out.println("5. задание:  " + ("Исходное выражение:  " + symb));
         String symb2 = symb.replace("?", "HELLO");
         System.out.println("             " + "Результат замены:    " + symb2.replace("#", ""));
-
+        double x = 10; double s = 2; double t = 2; // 6. задание
+        double y = Math.pow(Math.sin(Math.pow(x, t)), 2) / Math.sqrt(1 + Math.pow(x , t)); //sin в радианах
+        System.out.println("6. задание:  y=" + y);
 
     }
 }
